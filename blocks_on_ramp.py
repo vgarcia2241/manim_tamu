9675c3e110886b2542d630cee16c627145ecc77e
Viviana <vgarcia2241@tamu.edu>
2026-02-03 00:05:37 -0600

    xoom working :0

--- blocks_on_ramp.py
+++ blocks_on_ramp.py
@@ -58,6 +58,7 @@ class s1(Scene): #Initial scene
         theta_angle = Angle(theta_line, theta_line.copy().rotate(ramp_line.get_angle(), about_point=theta_line.get_right()), radius=1.25, other_angle=True, color=YELLOW)
         acceleration = Tex(r"What is the magnitude of\\ acceleration of the top block?", color=WHITE).to_edge(UP)
         motion = Arrow(block2.point_from_proportion(0.95), block2.point_from_proportion(0.95)+1.5*ramp_line.get_unit_vector(), buff=0, color=BLUE).shift([0,-0.75,0])
+        motion2 = Arrow(block1.point_from_proportion(p1), block1.point_from_proportion(p1)-1.5*ramp_line.get_unit_vector(), buff=0, color=WHITE)
         
         print(ramp_line.get_unit_vector())
         
@@ -154,3 +155,54 @@ class s1(Scene): #Initial scene
         
 #________________________________________________
 # end of paragraph 2
+
+'''
+Attempting to zoom into block1 (red)
+'''
+class s2(MovingCameraScene):
+    def construct(self):
+        #ramp initialization
+        ramp = Polygon([-3.5,-3,0],[-3.5,1,0],[3.5,-3,0], color=GRAY_C).set_stroke(width=0.1).set_fill(GRAY_C,opacity=1)
+        ramp_line = Line(ramp.get_critical_point([-1,1,0]), ramp.get_critical_point([1,-1,0]))
+        ramp_direction = np.reciprocal(ramp_line.get_unit_vector())
+        ramp_direction[2] = 0
+        ramp_direction[1] *= -1
+        ramp_direction = ramp_direction / np.linalg.norm(ramp_direction)
+        
+        normal = Arrow(ramp_line.point_from_proportion(0.5), ramp_line.point_from_proportion(0.5)+3*ramp_direction, buff=0)
+        
+        #setting up blocks
+        block_dim = [1,4,1,2] #setting dimensions here should allow for easy size changing
+        block2 = Rectangle(BLUE, block_dim[0], block_dim[1]).set_stroke(width=0.1).set_fill(BLUE, opacity=1)
+        block2.rotate(ramp_line.get_angle()).move_to(ramp_line.point_from_proportion(0.65)).shift(ramp_direction*block_dim[0]/2)
+        block1 = Rectangle(RED, block_dim[2], block_dim[3]).set_stroke(width=0.1).set_fill(RED, opacity=1)
+        block1.rotate(ramp_line.get_angle()).move_to(ramp_line.point_from_proportion(0.65)).shift(ramp_direction*((2*block_dim[0]+block_dim[2])/2))
+        
+        #creating pulley system
+        disk = Circle(0.5, GRAY_C).set_stroke(width=0.1).set_fill(GRAY_C,opacity=1).set_z_index(0.1)
+        disk.rotate(ramp_line.get_angle()).move_to(ramp_line.point_from_proportion(0.01)).shift(ramp_direction*block_dim[0])
+        stick = Line(ramp_line.point_from_proportion(0.01), disk.get_center(), color = GRAY_E).set_stroke(width=9).set_z_index(0.2)
+        #connection points of pulley
+        pointB = disk.point_from_proportion(0.75)
+        pointA = disk.point_from_proportion(0.25)
+        #conection points to blocks
+        p2 =  (0.5*block_dim[0] + block_dim[1]) / (2*block_dim[0] + 2*block_dim[1])
+        p1 = (0.5*block_dim[2] + block_dim[3]) / (2*block_dim[2] + 2*block_dim[3])
+        point2 = block2.point_from_proportion(p2)
+        point1 = block1.point_from_proportion(p1)
+        #pulley lines
+        line2 = Line(point2, pointB)
+        line1 = Line(point1,pointA)
+        
+        original = VGroup(ramp, block1, block2, disk, stick, line2, line1)
+        outlined = VGroup()
+        for i in original:
+            outlined.add(Outline(self,i))
+        outlined[1] = original[1]
+        
+        self.add(original)
+        self.wait()
+        self.play(
+            self.camera.frame.animate.move_to(block1.copy().shift([1,0,0])).set(width=3*block1.width),
+            TransformMatchingShapes(original,outlined)
+        )
